556aeb49128bbdac46c684431a0a45cfb90c0a3c
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-12 18:16:17 +0100

    Added classes and containers for buttons

--- calculator-project/src/App.jsx
+++ calculator-project/src/App.jsx
@@ -5,37 +5,36 @@ function App() {
     <>
       <div className="calculator-container">
         <div className="input-field">Input number</div>
-        <div className="key-container">
-          <div className="row-one">
-            <button>9</button>
-            <button>8</button>
-            <button>7</button>
-          </div>
 
-          <div className="row-two">
-            <button>6</button>
-            <button>5</button>
-            <button>4</button>
-          </div>
+        <div className="row-one">
+          <button className="num-btn">9</button>
+          <button className="num-btn">8</button>
+          <button className="num-btn">7</button>
+        </div>
 
-          <div className="row-three">
-            <button>3</button>
-            <button>2</button>
-            <button>1</button>
-          </div>
+        <div className="row-two">
+          <button className="num-btn">6</button>
+          <button className="num-btn">5</button>
+          <button className="num-btn">4</button>
+        </div>
 
-          <div className="row-four">
-            <button>=</button>
-            <button>.</button>
-            <button>0</button>
-          </div>
+        <div className="row-three">
+          <button className="num-btn">3</button>
+          <button className="num-btn">2</button>
+          <button className="num-btn">1</button>
+        </div>
 
-          <div className="operations">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>/</button>
-          </div>
+        <div className="row-four">
+          <button className="operation-btn">=</button>
+          <button className="operation-btn">.</button>
+          <button className="num-btn">0</button>
+        </div>
+
+        <div className="operations">
+          <button className="operation-btn">+</button>
+          <button className="operation-btn">-</button>
+          <button className="operation-btn">*</button>
+          <button className="operation-btn">/</button>
         </div>
       </div>
     </>
